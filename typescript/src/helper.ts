65cb166b138712f4ab6fa7209c0ab55a0e3d3bb9
durga parshad <durga.parshad@zemosolabs.com>
2023-02-17 13:10:51 +0530

    update new changes for utils

--- typescript/src/helper.ts
+++ typescript/src/helper.ts
@@ -1,4 +1,5 @@
 import { TaskProps, Task } from './task';
+import { TaskOperations } from './task_operations';
 
 export const splitFirstSpace = (s: string) => {
     var pos = s.indexOf(' ');
@@ -12,5 +13,43 @@ export const forEachProject = (func: (key: string, value: Task[]) => any, tasks 
     for(var key in tasks) {
         if(tasks.hasOwnProperty(key))
             func(key, tasks[key])
+    }
+}
+
+export const execute = (commandLine: string, taskOperation: TaskOperations, readline : any, tasks : TaskProps) => {
+
+    var commandRest = splitFirstSpace(commandLine);
+    var command = commandRest[0];
+    switch (command) {
+        case "show":
+            taskOperation.show(readline, tasks);
+            break;
+        case "today":
+            taskOperation.today(readline, tasks);
+            break;
+        case "add":
+            taskOperation.add(commandRest[1], readline, tasks);
+            break;
+        case "check":
+            taskOperation.check(commandRest[1], readline, tasks);
+            break;
+        case "uncheck":
+            taskOperation.uncheck(commandRest[1], readline, tasks);
+            break;
+        case "help":
+            taskOperation.help(readline);
+            break;
+        case "deadline":
+            taskOperation.deadline(commandRest[1], readline, tasks);
+            break;
+        case "view":
+            taskOperation.view(commandRest[1], readline, tasks);
+            break;
+        case "delete":
+            taskOperation.deleteTask(commandRest[1], readline, tasks);
+            break;
+        default:
+            taskOperation.error(command, readline);
+            break;
     }
 }
--- typescript/src/task_list.ts
+++ typescript/src/task_list.ts
@@ -1,76 +1,38 @@
 /// <reference path="../typings/node/node.d.ts" />
 
 import { Task } from './task';
-import { splitFirstSpace } from './helper';
+import { execute } from './helper';
 import { TaskOperations } from './task_operations';
 import { createReadlineInterface, readLineOnClose } from './readlinehelper';
 
 
 export class TaskList
 {
-    static QUIT = 'quit';
-    private readline;
+    private static QUIT = 'quit';
+    private readline : any;
     private tasks: {[index: string]: Task[]} = {};
+    private taskOperation: TaskOperations;
     
     constructor(reader: NodeJS.ReadableStream, writer: NodeJS.WritableStream) {
 
         this.readline = createReadlineInterface(reader, writer);
-
+        this.taskOperation = new TaskOperations();
         this.readline.setPrompt("> ");
         this.readline.on('line', (cmd) => {
             if(cmd == TaskList.QUIT) {
                 this.readline.close();
                 return;
             }
-            this.execute(cmd);
+            execute(cmd, this.taskOperation, this.readline, this.tasks);
             this.readline.prompt();
         });
 
+        
         readLineOnClose(writer, this.readline);
     }
 
     run() {
         this.readline.prompt();
-    }
-
-    execute(commandLine: string) {
-        
-        const TaskOperation = new TaskOperations();
-
-        var commandRest = splitFirstSpace(commandLine);
-        var command = commandRest[0];
-        switch (command) {
-            case "show":
-                TaskOperation.show(this.readline, this.tasks);
-                break;
-            case "today":
-                TaskOperation.today(this.readline, this.tasks);
-                break;
-            case "add":
-                TaskOperation.add(commandRest[1], this.readline, this.tasks);
-                break;
-            case "check":
-                TaskOperation.check(commandRest[1], this.readline, this.tasks);
-                break;
-            case "uncheck":
-                TaskOperation.uncheck(commandRest[1], this.readline, this.tasks);
-                break;
-            case "help":
-                TaskOperation.help(this.readline);
-                break;
-            case "deadline":
-                TaskOperation.deadline(commandRest[1], this.readline, this.tasks);
-                break;
-            case "view":
-                TaskOperation.view(commandRest[1], this.readline, this.tasks);
-                break;
-            case "delete":
-                TaskOperation.deleteTask(commandRest[1], this.readline, this.tasks);
-                break;
-            default:
-                TaskOperation.error(command, this.readline);
-                break;
-        }
     }
 
 
