e8d2073fcd0c6d403714a359d4e9b5aaab5a05d1
durga parshad <durga.parshad@zemosolabs.com>
2023-02-17 12:53:11 +0530

    update class in operations file

--- typescript/src/task_list.ts
+++ typescript/src/task_list.ts
@@ -2,7 +2,7 @@
 
 import { Task } from './task';
 import { splitFirstSpace } from './helper';
-import { show, deadline, today, add, check, uncheck, help, deleteTask, view, error } from './task_operations';
+import { TaskOperations } from './task_operations';
 import { createReadlineInterface, readLineOnClose } from './readlinehelper';
 
 
@@ -34,38 +34,41 @@ export class TaskList
     }
 
     execute(commandLine: string) {
+        
+        const TaskOperation = new TaskOperations();
+
         var commandRest = splitFirstSpace(commandLine);
         var command = commandRest[0];
         switch (command) {
             case "show":
-                show(this.readline, this.tasks);
+                TaskOperation.show(this.readline, this.tasks);
                 break;
             case "today":
-                today(this.readline, this.tasks);
+                TaskOperation.today(this.readline, this.tasks);
                 break;
             case "add":
-                add(commandRest[1], this.readline, this.tasks);
+                TaskOperation.add(commandRest[1], this.readline, this.tasks);
                 break;
             case "check":
-                check(commandRest[1], this.readline, this.tasks);
+                TaskOperation.check(commandRest[1], this.readline, this.tasks);
                 break;
             case "uncheck":
-                uncheck(commandRest[1], this.readline, this.tasks);
+                TaskOperation.uncheck(commandRest[1], this.readline, this.tasks);
                 break;
             case "help":
-                help(this.readline);
+                TaskOperation.help(this.readline);
                 break;
             case "deadline":
-                deadline(commandRest[1], this.readline, this.tasks);
+                TaskOperation.deadline(commandRest[1], this.readline, this.tasks);
                 break;
             case "view":
-                view(commandRest[1], this.readline, this.tasks);
+                TaskOperation.view(commandRest[1], this.readline, this.tasks);
                 break;
             case "delete":
-                deleteTask(commandRest[1], this.readline, this.tasks);
+                TaskOperation.deleteTask(commandRest[1], this.readline, this.tasks);
                 break;
             default:
-                error(command, this.readline);
+                TaskOperation.error(command, this.readline);
                 break;
         }
     }
--- typescript/src/task_operations.ts
+++ typescript/src/task_operations.ts
@@ -5,150 +5,172 @@ import { println } from "./readlinehelper";
 import { Task, TaskProps } from './task';
 
 
+export class TaskOperations {
 
-export const show = (readline : any,tasks : TaskProps) => {
-    forEachProject((project, taskList) => {
-        println(project, readline);
-        taskList.forEach((task) => {
-            println(util.format("    [%s] %s: %s %s", (task.done ? 'x' : ' '), task.id, task.description, task.deadline), readline);
-        });
-        println('',readline);
-    }, tasks);
-}
+    show = (readline : any,tasks : TaskProps) => {
 
-export const today = (readline : any,tasks : TaskProps) => {
-    forEachProject((project, taskList) => {
-        println(project, readline);
-        taskList.forEach((task) => {
-            if(moment(moment(task.deadline).format('YYYY-MM-DD')).isSame(moment().format('YYYY-MM-DD')))
-                println(util.format("    [%s] %s: %s %s", (task.done ? 'x' : ' '), task.id, task.description, task.deadline), readline);
-        });
-        println('', readline);
-    }, tasks);
-}
-
-export const deadline = (commandLine: string, readline: any, tasks: TaskProps) => {
-
-    var subcommandRest = splitFirstSpace(commandLine);
-    var idString = subcommandRest[0];
-    var date = new Date(subcommandRest[1]);
-    var found = false;
-    forEachProject((project, taskList) => {
-        taskList.forEach((task) => {
-            if (task.id == idString) {
-                task.deadline = date;
-                found = true
-            }
-        });
-    }, tasks);
-
-    if(!found)
-        println(util.format("Could not find a task with an ID of %s.", idString), readline);
-}
-
-export const add = (commandLine: string, readline: any , tasks : TaskProps) => {
-    var subcommandRest = splitFirstSpace(commandLine);
-    var subcommand = subcommandRest[0];
-    if (subcommand === "project") {
-        addProject(subcommandRest[1], tasks);
-    } else if (subcommand === "task") {
-        var projectTask = splitFirstSpace(subcommandRest[1]);
-        let idString = splitFirstSpace(projectTask[1])
-        addTask(projectTask[0], idString[0], idString[1], tasks, readline);
-    }
-}
-
-const addTask = (project: string, description: string, idString: string, tasks : TaskProps, readline: any) => {
-    var projectTasks = tasks[project];
-    
-    if (projectTasks == null) {
-        println(util.format("Could not find a project with the name \"%s\".", project), readline);
-        return;
-    }
-    let regString =  new RegExp(/[A-z0-9]*/);
-    if(regString.test(idString) == true){
-        projectTasks.push(new Task(idString , description, false, null));
-    }
-    else{
-        println(util.format("please make sure you are using number and letters without space and special char \"%s\".", idString), readline);
-        return;
-    }
-       
-}
-
-
-const addProject = (name: string, tasks: TaskProps) => {
-    tasks[name] = [];
-}
-
-export const view = (commandLine: string,  readline: any, tasks: TaskProps) => {
-    var subcommandRest = splitFirstSpace(commandLine);
-    var subCommand = subcommandRest[0];
-    if(subCommand == 'deadline'){
         forEachProject((project, taskList) => {
             println(project, readline);
-            taskList.sort(function(a: any, b: any ){return a._deadline - b._deadline}).forEach((task) => {
+            taskList.forEach((task) => {
                 println(util.format("    [%s] %s: %s %s", (task.done ? 'x' : ' '), task.id, task.description, task.deadline), readline);
             });
+            println('',readline);
+        }, tasks);
+
+    }
+
+    today = (readline : any,tasks : TaskProps) => {
+        forEachProject((project, taskList) => {
+            println(project, readline);
+            taskList.forEach((task) => {
+                if(moment(moment(task.deadline).format('YYYY-MM-DD')).isSame(moment().format('YYYY-MM-DD')))
+                    println(util.format("    [%s] %s: %s %s", (task.done ? 'x' : ' '), task.id, task.description, task.deadline), readline);
+            });
             println('', readline);
-        },tasks);
+        }, tasks);
+    }
+
+    deadline = (commandLine: string, readline: any, tasks: TaskProps) => {
+
+        var subcommandRest = splitFirstSpace(commandLine);
+        var idString = subcommandRest[0];
+        var date = new Date(subcommandRest[1]);
+        var found = false;
+        forEachProject((project, taskList) => {
+            taskList.forEach((task) => {
+                if (task.id == idString) {
+                    task.deadline = date;
+                    found = true
+                }
+            });
+        }, tasks);
+    
+        if(!found)
+            println(util.format("Could not find a task with an ID of %s.", idString), readline);
+    }
+
+    add = (commandLine: string, readline: any , tasks : TaskProps) => {
+        var subcommandRest = splitFirstSpace(commandLine);
+        var subcommand = subcommandRest[0];
+        if (subcommand === "project") {
+            this.addProject(subcommandRest[1], tasks);
+        } else if (subcommand === "task") {
+            var projectTask = splitFirstSpace(subcommandRest[1]);
+            let idString = splitFirstSpace(projectTask[1])
+            this.addTask(projectTask[0], idString[0], idString[1], tasks, readline);
+        }
+    }
+
+    addTask = (project: string, description: string, idString: string, tasks : TaskProps, readline: any) => {
+        var projectTasks = tasks[project];
+        
+        if (projectTasks == null) {
+            println(util.format("Could not find a project with the name \"%s\".", project), readline);
+            return;
+        }
+        let regString =  new RegExp(/[A-z0-9]*/);
+        if(regString.test(idString) == true){
+            projectTasks.push(new Task(idString , description, false, null));
+        }
+        else{
+            println(util.format("please make sure you are using number and letters without space and special char \"%s\".", idString), readline);
+            return;
+        }
+           
+    }
+
+    addProject = (name: string, tasks: TaskProps) => {
+        tasks[name] = [];
+    }
+
+    view = (commandLine: string,  readline: any, tasks: TaskProps) => {
+        var subcommandRest = splitFirstSpace(commandLine);
+        var subCommand = subcommandRest[0];
+        if(subCommand == 'deadline'){
+            forEachProject((project, taskList) => {
+                println(project, readline);
+                taskList.sort(function(a: any, b: any ){return a._deadline - b._deadline}).forEach((task) => {
+                    println(util.format("    [%s] %s: %s %s", (task.done ? 'x' : ' '), task.id, task.description, task.deadline), readline);
+                });
+                println('', readline);
+            },tasks);
+        }
+        
+    }
+
+    deleteTask = (idString: string, readline: any, tasks: TaskProps) => {
+
+        let found = false
+        forEachProject((project, taskList) => {
+            let newTasklist = taskList.splice(taskList.findIndex(a => a.id === idString) , 1)
+            if(newTasklist.length > 0){
+                found = true
+            }
+        }, tasks);
+    
+        if(!found)
+            println(util.format("Could not find a task with an ID of %s.", idString), readline);
+    }
+
+    check = (idString: string, readline : any, tasks: TaskProps) => {
+        this.setDone(idString, true, readline, tasks);
+    }
+    
+
+    uncheck = (idString: string, readline : any, tasks: TaskProps) => {
+        this.setDone(idString, false, readline, tasks);
+    }
+    
+    setDone = (idString: string, done: boolean, readline: any, tasks: TaskProps) => {
+        var found = false;
+        forEachProject((project, taskList) => {
+            taskList.forEach((task) => {
+                if (task.id == idString) {
+                    task.done = done;
+                    found = true;
+                }
+            });
+        }, tasks);
+        if(!found)
+            println(util.format("Could not find a task with an ID of %d.", idString), readline);
+    }
+
+    help = (readline) => {
+        println("Commands:", readline);
+        println("  show", readline);
+        println("  today", readline);
+        println("  add project <project name>", readline);
+        println("  add task <project name> <task description> <task ID>", readline);
+        println("  check <task ID>", readline);
+        println("  uncheck <task ID>", readline);
+        println("  deadline <task ID>", readline);
+        println("  delete <task ID>", readline);
+        println("  view deadline", readline);
+        println("", readline);
+    }
+    
+    error = (command: string, readline) => {
+        println('I don\'t know what the command "' + command + '" is.', readline);
     }
     
 }
 
 
-export const deleteTask = (idString: string, readline: any, tasks: TaskProps) => {
-
-    let found = false
-    forEachProject((project, taskList) => {
-        let newTasklist = taskList.splice(taskList.findIndex(a => a.id === idString) , 1)
-        if(newTasklist.length > 0){
-            found = true
-        }
-    }, tasks);
-
-    if(!found)
-        println(util.format("Could not find a task with an ID of %s.", idString), readline);
-}
 
 
-export const check = (idString: string, readline : any, tasks: TaskProps) => {
-    setDone(idString, true, readline, tasks);
-}
 
-export const uncheck = (idString: string, readline : any, tasks: TaskProps) => {
-    setDone(idString, false, readline, tasks);
-}
 
-const setDone = (idString: string, done: boolean, readline: any, tasks: TaskProps) => {
-    var found = false;
-    forEachProject((project, taskList) => {
-        taskList.forEach((task) => {
-            if (task.id == idString) {
-                task.done = done;
-                found = true;
-            }
-        });
-    }, tasks);
-    if(!found)
-        println(util.format("Could not find a task with an ID of %d.", idString), readline);
-}
 
-export const help = (readline) => {
-    println("Commands:", readline);
-    println("  show", readline);
-    println("  today", readline);
-    println("  add project <project name>", readline);
-    println("  add task <project name> <task description> <task ID>", readline);
-    println("  check <task ID>", readline);
-    println("  uncheck <task ID>", readline);
-    println("  deadline <task ID>", readline);
-    println("  delete <task ID>", readline);
-    println("  view deadline", readline);
-    println("", readline);
-}
 
-export const error = (command: string, readline) => {
-    println('I don\'t know what the command "' + command + '" is.', readline);
-}
+
+
+
+
+
+
+
+
+
+
+
 
